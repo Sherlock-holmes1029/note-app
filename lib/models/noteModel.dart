62521002e7c523d3e63e4c12a321b56f5ecb2485
Sherlock-holmes1029 <kishkramy@gmail.com>
2023-03-20 16:37:55 +0300

    add hive

--- lib/models/noteModel.dart
+++ lib/models/noteModel.dart
@@ -4,7 +4,12 @@ class NoteModel{
   final String date;
   final int color;
 
-  NoteModel({ required this.title, required this.subTitle, required this.date, required this.color});
+  NoteModel({
+    required this.title,
+    required this.subTitle,
+    required this.date,
+    required this.color
+  });
 
 
 }
